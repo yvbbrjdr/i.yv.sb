70dff5beaa8b3e0a26c6874a15396e84594d93b3
yvbbrjdr <xyyqzy@hotmail.com>
2026-05-29 03:45:58 -0700

    Update fetch response to return client IP and request details in JSON format

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,13 @@
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response("Hello World!");
+		return new Response(JSON.stringify({
+			ip: request.headers.get("CF-Connecting-IP"),
+			...request.cf,
+		}), {
+			headers: {
+				"Content-Type": "application/json"
+			},
+		});
 	},
 } satisfies ExportedHandler<Env>;
