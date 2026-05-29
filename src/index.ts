9c8ef66966ce39501c0ca0e7c096277efdf9b6e8
yvbbrjdr <xyyqzy@hotmail.com>
2026-05-29 11:11:57 -0700

    Format JSON response for better readability

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 export default {
   async fetch(request, env, ctx): Promise<Response> {
     return new Response(
-      JSON.stringify({
-        ip: request.headers.get("CF-Connecting-IP"),
-        ...request.cf,
-      }),
+      JSON.stringify(
+        {
+          ip: request.headers.get("CF-Connecting-IP"),
+          ...request.cf,
+        },
+        null,
+        2,
+      ),
       {
         headers: {
           "Content-Type": "application/json",
